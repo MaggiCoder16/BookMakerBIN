0c9aa7d61f75c1b40943461eecfb0c509aed775b
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-09-05 13:19:09 +0530

    Update smoothestbinmaker.py

--- smoothestbinmaker.py
+++ smoothestbinmaker.py
@@ -4,7 +4,7 @@ import chess.polyglot
 import datetime
 
 # Maximum number of moves from each game to include in the book
-MAX_BOOK_PLIES = 200
+MAX_BOOK_PLIES = 1000
 # Internal weight normalization target
 MAX_BOOK_WEIGHT = 1000000
 # Maximum weight allowed in Polyglot entry (16-bit)
@@ -165,4 +165,4 @@ def build_book_file(pgn_path, book_path):
 
 if __name__ == "__main__":
    # build_book_file("SamplePGN.pgn", "lila.bin")
-    build_book_file("drawing.pgn", "drawing.bin")
+    build_book_file("mydrawbook.pgn", "slowdraw.bin")
